3bbbb49533a15cc1442d772f54565edf7933fbdf
atka55 <nagyatko55@gmail.com>
2023-02-22 11:22:23 +0100

    cucc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 const allomasok = ['Nyugati','Zugló','Kőbánya-Kispest','Ferihegy','Vecsés',
-'Üllő','Monor','Monorierdő','Pilis','Albertisa','Ceglédbercel','Abony','Szolnok','Szajol',
+'Üllő','Monor','Monorierdő','Pilis','Albertisa','Cegléd','Ceglédbercel','Abony','Szolnok','Szajol',
 'Törökszentmiklós','Fegyvernek-Örményes','Kisújszállás']
 
 var a = document.getElementById("itt");
 a.max = allomasok.length-1;
+
+document.getElementById("vonat").style.left="175px"
+
+
 
 
 
@@ -14,6 +18,9 @@ function elotteutana(megallo)
 
     var ut = document.getElementById("itt");
     ut.value = p;
+
+
+    
 
     
     if (p==-1)
@@ -38,6 +45,9 @@ function elotteutana(megallo)
         else{
             r += "Utána: "+allomasok[p+1];
         }
+        var poz = 700/allomasok.length*p
+        console.log(poz)
+        document.getElementById("vonat").style.left=(poz+70)+"px"
 
     }
     return r;
